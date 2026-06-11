57a87153cf8e55e31d90a9727e6b059958f02f08
mhmdgasm929-ui <mhmdgasm929@gmail.com>
2026-06-11 08:48:26 +0300

    Add start command handler to bot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import telebot
 
-# 🔑 حط توكن البوت هنا
 TOKEN = "8737122794:AAFR51dPCPLolWdz_P3DFRN_JbvWagFkFcM"
 
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "البوت شغال 🔥")
+
+bot.polling()
